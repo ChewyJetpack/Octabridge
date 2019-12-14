2ed8147ed5e82be51b3b4aeb8d3305e3dc2102a8
Emil Smith <drkrepz@gmail.com>
2019-12-14 22:41:48 +0000

    stater

---
+++ src/js/index.js
@@ -0,0 +1,8 @@
+/* eslint-disable */
+
+import "file-loader?name=[name].[ext]!../index.html";
+import "../style/index.scss";
+
+window.onload = function() {};
+
+console.log("Hello Rigo from the console!");
